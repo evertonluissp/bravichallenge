11c7e21760b8e9520bbc1c32ab5aa20dee36b6eb
Everton Luis <evertonluissp@gmail.com>
2020-02-12 02:38:21 -0300

    BracketsService: created with method isBalancedBrackets(String)

---
+++ src/main/java/com/evertonluissp/wirecardchallenge/services/BracketsService.java
@@ -0,0 +1,49 @@
+package com.evertonluissp.wirecardchallenge.services;
+
+import org.springframework.stereotype.Service;
+
+import java.util.regex.Pattern;
+
+import static com.evertonluissp.wirecardchallenge.utils.StringUtils.removeFirstAndLastCharacter;
+import static java.util.regex.Pattern.compile;
+
+@Service
+public class BracketsService {
+
+    private static Pattern patternForBrackets = compile("\\[.*\\]");
+    private static Pattern patternForBraces = compile("\\{.*\\}");
+    private static Pattern patternForParentheses = compile("\\(.*\\)");
+
+    public boolean isBalancedBrackets(String input) {
+        if (input == null || input.isBlank()) {
+            return true;
+        }
+
+        var matcherForBrackets = patternForBrackets.matcher(input);
+        String inputIntoBrackets = null;
+        if (matcherForBrackets.find()) {
+            inputIntoBrackets = removeFirstAndLastCharacter(matcherForBrackets.group());
+            input = input.replace(matcherForBrackets.group(), "");
+        }
+
+        var matcherForBraces = patternForBraces.matcher(input);
+        String inputIntoBraces = null;
+        if (matcherForBraces.find()) {
+            inputIntoBraces = removeFirstAndLastCharacter(matcherForBraces.group());
+            input = input.replace(matcherForBraces.group(), "");
+        }
+
+        var matcherForParentheses = patternForParentheses.matcher(input);
+        String inputIntoParentheses = null;
+        if (matcherForParentheses.find()) {
+            inputIntoParentheses = removeFirstAndLastCharacter(matcherForParentheses.group());
+            input = input.replace(matcherForParentheses.group(), "");
+        }
+
+        return input.isBlank() &&
+                isBalancedBrackets(inputIntoBrackets) &&
+                isBalancedBrackets(inputIntoBraces) &&
+                isBalancedBrackets(inputIntoParentheses);
+    }
+
+}
