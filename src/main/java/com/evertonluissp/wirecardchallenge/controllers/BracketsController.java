2f60cd0445c1af105327cb490cc9f2695a059dcf
Everton Luis <evertonluissp@gmail.com>
2020-02-12 02:39:45 -0300

    BracketsController: created with GET /?input=String

---
+++ src/main/java/com/evertonluissp/wirecardchallenge/controllers/BracketsController.java
@@ -0,0 +1,28 @@
+package com.evertonluissp.wirecardchallenge.controllers;
+
+import com.evertonluissp.wirecardchallenge.services.BracketsService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.ResponseEntity.noContent;
+import static org.springframework.http.ResponseEntity.ok;
+
+@RestController
+@RequestMapping("brackets")
+public class BracketsController {
+
+    public final BracketsService service;
+
+    public BracketsController(BracketsService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public ResponseEntity<?> isValid(@RequestParam String input) {
+        return service.isBalancedBrackets(input) ? ok().build() : noContent().build();
+    }
+
+}
