12e2e702a109a395c529819e796df91f33e98c11
Everton Luis <evertonluissp@gmail.com>
2020-02-12 03:06:58 -0300

    BracketsServiceTest: created with isBalancedBrackets_validInputs_shouldReturnsTrue

---
+++ src/test/java/com/evertonluissp/wirecardchallenge/services/BracketsServiceTest.java
@@ -0,0 +1,31 @@
+package com.evertonluissp.wirecardchallenge.services;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringJUnitConfig(classes = BracketsService.class)
+public class BracketsServiceTest {
+
+    @Autowired
+    private BracketsService service;
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "()",
+            "{}",
+            "[]",
+            "()[]",
+            "(){}",
+            "[]{}",
+            "(){}[]",
+            "[{()}](){}"
+    })
+    public void isBalancedBrackets_validInputs_shouldReturnsTrue(String input) {
+        assertThat(service.isBalancedBrackets(input)).isTrue();
+    }
+
+}
