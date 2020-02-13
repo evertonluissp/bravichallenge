eb4d17197fff5e839e9a8e68490f7f312a015150
Everton Luis <evertonluissp@gmail.com>
2020-02-12 22:25:29 -0300

    Person & Contact: add hashCode equals

--- src/main/java/com/evertonluissp/bravichallenge/entities/Contact.java
+++ src/main/java/com/evertonluissp/bravichallenge/entities/Contact.java
@@ -2,10 +2,25 @@ package com.evertonluissp.bravichallenge.entities;
 
 import com.evertonluissp.bravichallenge.enums.ContactType;
 
+import static java.util.Objects.hash;
+
 public class Contact {
 
     public int id;
     public String value;
     public ContactType type;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        var contact = (Contact) o;
+        return id == contact.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return hash(id);
+    }
 
 }
--- src/main/java/com/evertonluissp/bravichallenge/entities/Person.java
+++ src/main/java/com/evertonluissp/bravichallenge/entities/Person.java
@@ -2,10 +2,25 @@ package com.evertonluissp.bravichallenge.entities;
 
 import java.util.List;
 
+import static java.util.Objects.hash;
+
 public class Person {
 
     public int id;
     public String name;
     public List<Contact> contacts;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        var person = (Person) o;
+        return id == person.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return hash(id);
+    }
 
 }
